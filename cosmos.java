7a2ecd4651bd14f10c36255c8df8b130c56726b2
davidju27543 <100405824@alumnos.uc3m.es>
2020-12-04 20:18:27 +0100

    Few changes in main

--- cosmos.java
+++ cosmos.java
@@ -1,18 +1,22 @@
 //Main of the program
 //once exported it should be executed as ./Cosmos.sh <problema.prob> <heuristica>
-
-
-
 //main recieves problema and heuristica FIXME: no se why no va
-public static void main(String[3] args){
+public class cosmos{
 
-    //TODO: Control de errores de numero de argumentos.
+    public static void main(String[] args){ //No se puede poner dentro del corchete la longitud de args, para eso usamos control de errores
 
-    //First, we read problema.prob and get the data info
-    problema = new problema(args[1]); 
-
-    //then we setup the heuristic
-
-    //create the initial state. Esto sería con h = 0. Cómo crear estados? No necesitan toda la info del problema.
-
+        //TODO: Control de errores de numero de argumentos.
+        System.out.println("Hello world¡");
+        //First, we read problema.prob and get the data info
+        
+        for(int i = 0; i<args.length;i++){
+            System.out.println(args[i]);
+        }
+        //problema = new problema(args[1]); 
+    
+        //then we setup the heuristic
+    
+        //create the initial state. Esto sería con h = 0. Cómo crear estados? No necesitan toda la info del problema.
+    
+    }
 }
